30dc9b6158658728101182179b3ecb99ef3660cf
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-19 17:42:46 +0530

    Event admin panel

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,2 +1,17 @@
 ActiveAdmin.register Event do
+  permit_params :name, :description, :starts_at, :ends_at, :place_id, :display_picture
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :description
+    column :display_picture do |product|
+      image_tag product.display_picture.thumb.url
+    end
+    column :place
+    column :starts_at
+    column :ends_at
+    actions
+  end
 end
