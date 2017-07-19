7170c80be4cbf51b1aff7882fedc41ff832a42d9
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-19 16:59:06 +0530

    Set home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   get 'pages/home'
 
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
