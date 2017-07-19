0a2f96b51b68915fbce88277ea7379e73e2d34e9
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-19 17:20:00 +0530

    Place model validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,7 @@
 class Place < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true, uniqueness: { scope: :longitude }
+
+  enum kind: { track: 0, evillage: 1, utility: 2 }
 end
