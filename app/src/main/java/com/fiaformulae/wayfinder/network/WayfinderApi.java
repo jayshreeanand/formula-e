cd8a0adc70ff692560edc357396b7700ea2769cb
Naveen <naveen246@gmail.com>
2017-07-21 16:40:34 +0530

    Call api end points

--- app/src/main/java/com/fiaformulae/wayfinder/network/WayfinderApi.java
+++ app/src/main/java/com/fiaformulae/wayfinder/network/WayfinderApi.java
@@ -1,7 +1,13 @@
 package com.fiaformulae.wayfinder.network;
 
 import com.fiaformulae.wayfinder.Wayfinder;
+import com.fiaformulae.wayfinder.models.Event;
+import com.fiaformulae.wayfinder.models.Place;
+import java.util.ArrayList;
 import okhttp3.OkHttpClient;
+import rx.observables.ConnectableObservable;
+
+import static com.fiaformulae.wayfinder.utils.RxUtils.applyIOToMainThreadSchedulers;
 
 public class WayfinderApi {
   private static WayfinderService wayfinderService;
@@ -13,5 +19,13 @@ public class WayfinderApi {
 
   public static WayfinderApi getInstance() {
     return new WayfinderApi(Wayfinder.getBaseUrl(), OkHttpHelper.getOkHttpClientInstance());
+  }
+
+  public ConnectableObservable<ArrayList<Event>> getEvents() {
+    return wayfinderService.getEvents().compose(applyIOToMainThreadSchedulers()).replay();
+  }
+
+  public ConnectableObservable<ArrayList<Place>> getPlaces() {
+    return wayfinderService.getPlaces().compose(applyIOToMainThreadSchedulers()).replay();
   }
 }
--- app/src/main/java/com/fiaformulae/wayfinder/network/WayfinderService.java
+++ app/src/main/java/com/fiaformulae/wayfinder/network/WayfinderService.java
@@ -1,5 +1,14 @@
 package com.fiaformulae.wayfinder.network;
 
+import com.fiaformulae.wayfinder.models.Event;
+import com.fiaformulae.wayfinder.models.Place;
+import java.util.ArrayList;
+import retrofit2.http.GET;
+import rx.Observable;
+
 public interface WayfinderService {
 
+  @GET("events") Observable<ArrayList<Event>> getEvents();
+
+  @GET("places") Observable<ArrayList<Place>> getPlaces();
 }
