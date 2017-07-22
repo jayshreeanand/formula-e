056d24937005a342e09b351ac4ffe0230ab49221
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-22 16:41:43 +0530

    Admin panel -team params

--- app/admin/team.rb
+++ app/admin/team.rb
@@ -1,13 +1,19 @@
 ActiveAdmin.register Team do
-  permit_params :name, :description, :statistics, :display_picture
+  permit_params :name, :description, :statistics, :display_picture, :logo, :flag
 
   index do
     selectable_column
     id_column
     column :name
     column :description
-    column :display_picture do |product|
-      image_tag product.display_picture.thumb.url
+    column :display_picture do |team|
+      image_tag team.display_picture.thumb.url
+    end
+    column :logo do |team|
+      image_tag team.logo.thumb.url
+    end
+    column :flag do |team|
+      image_tag team.flag.thumb.url
     end
     column :statistics
     actions
