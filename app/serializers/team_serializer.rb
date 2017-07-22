de47974966b76a75ada9358dba2fddf0791b45f8
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-22 19:52:21 +0530

    Team serializer

--- app/serializers/team_serializer.rb
+++ app/serializers/team_serializer.rb
@@ -1,7 +1,15 @@
 class TeamSerializer < BaseSerializer
-  attributes :name, :description, :starts_at, :ends_at, :display_picture
+  attributes :name, :description, :starts_at, :ends_at, :display_picture, :logo, :flag
 
   def display_picture
     object.display_picture_url(:normal)
+  end
+
+  def display_picture
+    object.logo_url(:normal)
+  end
+
+  def display_picture
+    object.flag_url(:normal)
   end
 end
