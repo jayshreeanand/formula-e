e6429adf939162602dd32c50116bd91b25b8ed3b
Naveen <naveen246@gmail.com>
2017-07-22 20:53:12 +0530

    Add eVillage geoJson

--- app/src/main/java/com/fiaformulae/wayfinder/sidebar/map/MapFragment.java
+++ app/src/main/java/com/fiaformulae/wayfinder/sidebar/map/MapFragment.java
@@ -1,11 +1,10 @@
 package com.fiaformulae.wayfinder.sidebar.map;
 
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,21 +12,15 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.fiaformulae.wayfinder.MainActivity;
 import com.fiaformulae.wayfinder.R;
+import com.fiaformulae.wayfinder.utils.GeoJsonUtils;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.services.commons.utils.TextUtils;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 public class MapFragment extends Fragment implements MapContract.View, OnMapReadyCallback {
   private static final String TAG = "MapFragment";
@@ -94,50 +87,13 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
   @Override public void onMapReady(MapboxMap mapboxMap) {
     this.mapboxMap = mapboxMap;
     new DrawTrackGeoJson().execute();
+    new DrawEVillageGeoJson().execute();
   }
 
   private class DrawTrackGeoJson extends AsyncTask<Void, Void, List<LatLng>> {
     @Override protected List<LatLng> doInBackground(Void... voids) {
 
-      ArrayList<LatLng> points = new ArrayList<>();
-
-      try {
-        // Load GeoJSON file
-        InputStream inputStream = getActivity().getAssets().open("track.geojson");
-        BufferedReader rd =
-            new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-        StringBuilder sb = new StringBuilder();
-        int cp;
-        while ((cp = rd.read()) != -1) {
-          sb.append((char) cp);
-        }
-
-        inputStream.close();
-
-        // Parse JSON
-        JSONObject json = new JSONObject(sb.toString());
-        JSONArray features = json.getJSONArray("features");
-        JSONObject feature = features.getJSONObject(0);
-        JSONObject geometry = feature.getJSONObject("geometry");
-        if (geometry != null) {
-          String type = geometry.getString("type");
-
-          // Our GeoJSON only has one feature: a line string
-          if (!TextUtils.isEmpty(type) && type.equalsIgnoreCase("LineString")) {
-
-            // Get the Coordinates
-            JSONArray coords = geometry.getJSONArray("coordinates");
-            for (int lc = 0; lc < coords.length(); lc++) {
-              JSONArray coord = coords.getJSONArray(lc);
-              LatLng latLng = new LatLng(coord.getDouble(1), coord.getDouble(0));
-              points.add(latLng);
-            }
-          }
-        }
-      } catch (Exception exception) {
-        Log.e(TAG, "Exception Loading GeoJSON: " + exception.toString());
-      }
-
+      ArrayList<LatLng> points = GeoJsonUtils.parseCoordinates(getActivity(), "track.geojson");
       return points;
     }
 
@@ -145,10 +101,29 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
       super.onPostExecute(points);
 
       if (points.size() > 0) {
-
         // Draw polyline on map
-        mapboxMap.addPolyline(
-            new PolylineOptions().addAll(points).color(Color.parseColor("#FFFFFF")).width(4));
+        mapboxMap.addPolyline(new PolylineOptions().addAll(points)
+            .color(ContextCompat.getColor(getActivity(), R.color.white))
+            .width(4));
+      }
+    }
+  }
+
+  private class DrawEVillageGeoJson extends AsyncTask<Void, Void, List<LatLng>> {
+    @Override protected List<LatLng> doInBackground(Void... voids) {
+
+      ArrayList<LatLng> points = GeoJsonUtils.parseCoordinates(getActivity(), "evillage.geojson");
+      return points;
+    }
+
+    @Override protected void onPostExecute(List<LatLng> points) {
+      super.onPostExecute(points);
+
+      if (points.size() > 0) {
+        // Draw polyline on map
+        mapboxMap.addPolyline(new PolylineOptions().addAll(points)
+            .color(ContextCompat.getColor(getActivity(), R.color.colorAccent))
+            .width(4));
       }
     }
   }
