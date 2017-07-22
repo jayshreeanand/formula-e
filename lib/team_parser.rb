7a5a2d9f566a61a269a8969004ea6975a92786ce
Jayshree Anandakumar <jayshreeanand90@gmail.com>
2017-07-22 19:50:01 +0530

    Team parser

--- lib/team_parser.rb
+++ lib/team_parser.rb
@@ -3,20 +3,41 @@ require 'open-uri'
 
 class TeamParser
   def self.run
-    doc = Nokogiri::HTML(open('http://www.fiaformulae.com/en/championship/teams-and-drivers'))
-    result = []
-    puts "### Search for nodes by css"
+    # doc = Nokogiri::HTML(open('https://s3.amazonaws.com/visualize-staging/uploads/static/teams-and-drivers.html'))
+    base_url = "http://localhost:3005"
+
+    doc = Nokogiri::HTML(open(URI.join(base_url, 'teams-and-drivers.html').to_s))
+
+    results = []
     doc.css('.m-teams-hub__wrap .b-card-item').each do |team|
-      result[:flag] = team.css('.s-flag img').attribute('src').value
-      result[:logo] = team.css('.s-text__top img').attribute('src').value
-      result[:display_picture] = team.css('.s-media img').attribute('src').value
+      result = {}
+      result[:flag] = URI.join(base_url, team.css('.s-flag img').attribute('src').value).to_s
+      result[:logo] = URI.join(base_url, team.css('.s-text__top img').attribute('src').value).to_s
+      result[:display_picture] = URI.join(base_url, team.css('.s-media img').attribute('src').value).to_s
       result[:name] = team.css('.s-team-name').text
       result[:description] = team.css('.s-short-biografy p').text
       statistics = {}
-      # statistics[:wins] = team.css('.js-team-details .s-stats .s-wins').text
-      # statistics[:podiums] = 
 
-      # team.css('.js-team-details .s-stats .s-item')
+
+      statistics[:wins] = team.css('.js-team-details .s-stats .s-wins').text.strip.split("\n")[0]
+      statistics[:podiums] = team.css('.js-team-details .s-stats .s-wins').text.strip.split("\n")[0]
+      team.css('.js-team-details .s-stats .s-item').each do |stat|
+        statistics[stat.children[1].text.parameterize] = stat.children[3].text
+      end
+      result[:statistics] = statistics
+      results << result
+    end
+
+    Team.all.destroy_all
+    results.each do |team_attributes|
+      team = Team.new
+      team.remote_flag_url = team_attributes[:flag]
+      team.remote_logo_url = team_attributes[:logo]
+      team.remote_display_picture_url = team_attributes[:display_picture]
+      team.name = team_attributes[:name]
+      team.description = team_attributes[:description]
+      team.statistics = team_attributes[:statistics]
+      team.save!
     end
   end
 end
