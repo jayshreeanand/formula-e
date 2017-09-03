73b894666b6ec8425be2098b61996ec5e58d5464
Naveen <naveen246@gmail.com>
2017-09-03 21:56:03 +0530

    Refactor

---
+++ app/src/main/java/com/fiaformulae/wayfinder/sidebar/map/Map.java
@@ -0,0 +1,174 @@
+package com.fiaformulae.wayfinder.sidebar.map;
+
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
+import com.fiaformulae.wayfinder.R;
+import com.fiaformulae.wayfinder.models.Place;
+import com.mapbox.mapboxsdk.annotations.Icon;
+import com.mapbox.mapboxsdk.annotations.IconFactory;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.Polyline;
+import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngBounds;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.services.api.directions.v5.DirectionsCriteria;
+import com.mapbox.services.api.directions.v5.MapboxDirections;
+import com.mapbox.services.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.services.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.services.commons.geojson.LineString;
+import com.mapbox.services.commons.models.Position;
+import java.util.ArrayList;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.fiaformulae.wayfinder.AppConstants.CURRENT_LOCATION;
+import static com.fiaformulae.wayfinder.AppConstants.USER_LATITUDE;
+import static com.fiaformulae.wayfinder.AppConstants.USER_LONGITUDE;
+import static com.mapbox.services.Constants.PRECISION_6;
+
+public class Map {
+  private static final String TAG = "MAP";
+  public MapboxMap mapboxMap;
+  public ArrayList<Marker> markers = new ArrayList<>();
+  public Place userLocation = new Place();
+  public Marker currentMarker;
+  public DirectionsRoute currentRoute = null;
+  public Polyline currentRoutePolyline;
+  public Context context;
+  public MapListener listener;
+
+  public Map(Context context, MapboxMap mapboxMap, MapListener listener) {
+    this.context = context;
+    this.mapboxMap = mapboxMap;
+    this.listener = listener;
+
+    mapboxMap.setOnMarkerClickListener(marker -> {
+      animateCameraToPosition(marker.getPosition());
+      currentMarker = marker;
+      listener.onMarkerClicked(marker.getTitle());
+      return true;
+    });
+  }
+
+  public void setCurrentLocation() {
+    userLocation.setLatitude(USER_LATITUDE);
+    userLocation.setLongitude(USER_LONGITUDE);
+    userLocation.setName(CURRENT_LOCATION);
+  }
+
+  public void showMarkerOnUserLocation() {
+    IconFactory iconFactory = IconFactory.getInstance(context);
+    Icon icon = iconFactory.fromResource(R.drawable.ic_marker_blue);
+    Marker marker = mapboxMap.addMarker(new MarkerOptions().position(userLocation.getLatLng())
+        .title(userLocation.getName())
+        .icon(icon));
+    markers.add(marker);
+  }
+
+  public void animateCameraToPosition(LatLng latLng) {
+    CameraPosition position = new CameraPosition.Builder().target(latLng).tilt(30).build();
+    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
+  }
+
+  public void showRoute() {
+    Position origin =
+        Position.fromCoordinates(userLocation.getLongitude(), userLocation.getLatitude());
+    Position destination = Position.fromCoordinates(currentMarker.getPosition().getLongitude(),
+        currentMarker.getPosition().getLatitude());
+    MapboxDirections client = new MapboxDirections.Builder().setAccessToken(
+        context.getString(R.string.mapbox_access_token))
+        .setOrigin(origin)
+        .setDestination(destination)
+        .setOverview(DirectionsCriteria.OVERVIEW_FULL)
+        .setProfile(DirectionsCriteria.PROFILE_WALKING)
+        .setAnnotation(DirectionsCriteria.ANNOTATION_DISTANCE,
+            DirectionsCriteria.ANNOTATION_DURATION)
+        .build();
+
+    client.enqueueCall(new Callback<DirectionsResponse>() {
+      @Override
+      public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
+        Log.d(TAG, call.request().url().toString());
+        if (response.body() == null || response.body().getRoutes().size() < 1) {
+          Log.e(TAG, "No routes found, make sure you set the right user and access token.");
+          return;
+        }
+        currentRoute = response.body().getRoutes().get(0);
+        Toast.makeText(context, "Distance: " + currentRoute.getDistance(), Toast.LENGTH_SHORT)
+            .show();
+        drawRoute(currentRoute);
+      }
+
+      @Override public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
+        Log.e(TAG, "Error: " + throwable.getMessage());
+        Toast.makeText(context, "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+
+  public void drawRoute(DirectionsRoute route) {
+    // Convert LineString coordinates into LatLng[]
+    LineString lineString = LineString.fromPolyline(route.getGeometry(), PRECISION_6);
+    List<Position> coordinates = lineString.getCoordinates();
+    List<LatLng> points = new ArrayList<>();
+    for (Position position : coordinates) {
+      points.add(new LatLng(position.getLatitude(), position.getLongitude()));
+    }
+
+    if (currentRoutePolyline != null) mapboxMap.removePolyline(currentRoutePolyline);
+    currentRoutePolyline = addPolyline(R.color.lochmara, 3, points);
+    showMarkerOnUserLocation();
+    ArrayList<LatLng> locations = new ArrayList<>();
+    locations.add(userLocation.getLatLng());
+    locations.add(currentMarker.getPosition());
+    fitCameraWithinBounds(locations);
+    listener.onDrawRoute();
+  }
+
+  public Polyline addPolyline(int color, int width, List<LatLng> points) {
+    return mapboxMap.addPolyline(new PolylineOptions().addAll(points)
+        .color(ContextCompat.getColor(context, color))
+        .width(width));
+  }
+
+  public void showMarkers(ArrayList<Place> placeList) {
+    clearMarkers();
+    ArrayList<LatLng> locations = new ArrayList<>();
+    for (Place place : placeList) {
+      locations.add(place.getLatLng());
+      Marker marker = mapboxMap.addMarker(
+          new MarkerOptions().position(place.getLatLng()).title(place.getName()));
+      markers.add(marker);
+    }
+    fitCameraWithinBounds(locations);
+  }
+
+  public void fitCameraWithinBounds(ArrayList<LatLng> locations) {
+    LatLngBounds.Builder latLngBoundsBuilder = new LatLngBounds.Builder();
+    for (LatLng location : locations) {
+      latLngBoundsBuilder.include(location);
+    }
+    LatLngBounds latLngBounds = latLngBoundsBuilder.build();
+    mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 250), 2000);
+  }
+
+  public void clearMarkers() {
+    for (Marker marker : markers) {
+      mapboxMap.removeMarker(marker);
+    }
+  }
+
+  public interface MapListener {
+    public void onDrawRoute();
+
+    public void onMarkerClicked(String title);
+  }
+}
--- app/src/main/java/com/fiaformulae/wayfinder/sidebar/map/MapFragment.java
+++ app/src/main/java/com/fiaformulae/wayfinder/sidebar/map/MapFragment.java
@@ -1,21 +1,18 @@
 package com.fiaformulae.wayfinder.sidebar.map;
 
 import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -24,42 +21,22 @@ import com.fiaformulae.wayfinder.R;
 import com.fiaformulae.wayfinder.models.Place;
 import com.fiaformulae.wayfinder.utils.GeoJsonUtils;
 import com.mapbox.mapboxsdk.Mapbox;
-import com.mapbox.mapboxsdk.annotations.Icon;
-import com.mapbox.mapboxsdk.annotations.IconFactory;
-import com.mapbox.mapboxsdk.annotations.Marker;
-import com.mapbox.mapboxsdk.annotations.MarkerOptions;
-import com.mapbox.mapboxsdk.annotations.Polyline;
-import com.mapbox.mapboxsdk.annotations.PolylineOptions;
-import com.mapbox.mapboxsdk.camera.CameraPosition;
-import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.geometry.LatLngBounds;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
-import com.mapbox.services.api.directions.v5.DirectionsCriteria;
-import com.mapbox.services.api.directions.v5.MapboxDirections;
-import com.mapbox.services.api.directions.v5.models.DirectionsResponse;
-import com.mapbox.services.api.directions.v5.models.DirectionsRoute;
-import com.mapbox.services.commons.geojson.LineString;
-import com.mapbox.services.commons.models.Position;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import java.util.ArrayList;
 import java.util.List;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static com.fiaformulae.wayfinder.AppConstants.CURRENT_LOCATION;
 import static com.fiaformulae.wayfinder.AppConstants.PLACES;
 import static com.fiaformulae.wayfinder.AppConstants.PLACE_FOOD;
 import static com.fiaformulae.wayfinder.AppConstants.PLACE_GAMING;
 import static com.fiaformulae.wayfinder.AppConstants.PLACE_WASHROOM;
-import static com.fiaformulae.wayfinder.AppConstants.USER_LATITUDE;
-import static com.fiaformulae.wayfinder.AppConstants.USER_LONGITUDE;
-import static com.mapbox.services.Constants.PRECISION_6;
 
-public class MapFragment extends Fragment implements MapContract.View, OnMapReadyCallback {
+public class MapFragment extends Fragment
+    implements MapContract.View, OnMapReadyCallback, Map.MapListener {
   private static final String TAG = "MapFragment";
   @BindView(R.id.sliding_layout) SlidingUpPanelLayout slidingLayout;
   @BindView(R.id.mapview) MapView mapView;
@@ -72,14 +49,9 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
   @BindView(R.id.location_name) TextView locationText;
   @BindView(R.id.direction_button) Button directionButton;
   private MapContract.Presenter presenter;
-  private MapboxMap mapboxMap;
   private List<Place> places;
   private boolean isFabOpen = false;
-  private ArrayList<Marker> markers = new ArrayList<>();
-  private Place userLocation = new Place();
-  private Marker currentMarker;
-  private DirectionsRoute currentRoute = null;
-  private Polyline currentRoutePolyline;
+  private Map map;
 
   public static MapFragment newInstance(ArrayList<Place> markerPlaces) {
     MapFragment fragment = new MapFragment();
@@ -116,7 +88,6 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
   @Override public void onResume() {
     super.onResume();
     mapView.onResume();
-    setCurrentLocation();
     slidingLayout.setFadeOnClickListener(
         view -> slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN));
     slidingLayout.addPanelSlideListener(onSlideListener());
@@ -154,26 +125,14 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
   }
 
   @Override public void onMapReady(MapboxMap mapboxMap) {
-    this.mapboxMap = mapboxMap;
+    map = new Map(getActivity(), mapboxMap, this);
+    map.setCurrentLocation();
     new DrawTrackGeoJson().execute();
     new DrawEVillageGeoJson().execute();
     if (getArguments() != null && getArguments().getSerializable(PLACES) != null) {
       ArrayList<Place> markerPlaces = (ArrayList<Place>) getArguments().getSerializable(PLACES);
-      showMarkers(markerPlaces);
+      map.showMarkers(markerPlaces);
     }
-    mapboxMap.setOnMarkerClickListener(marker -> {
-      animateCameraToPosition(marker.getPosition());
-      currentMarker = marker;
-      locationText.setText(marker.getTitle());
-      directionButton.setVisibility(View.VISIBLE);
-      if (slidingLayout.getPanelState() == SlidingUpPanelLayout.PanelState.HIDDEN) {
-        slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
-      }
-      if (CURRENT_LOCATION.equalsIgnoreCase(marker.getTitle())) {
-        directionButton.setVisibility(View.GONE);
-      }
-      return true;
-    });
   }
 
   @Override public void showProgressBar() {
@@ -198,123 +157,26 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
 
   @OnClick(R.id.fab_washroom) void onFabWashroomClick() {
     ArrayList<Place> washrooms = presenter.getPlacesContainingString(places, PLACE_WASHROOM);
-    showMarkers(washrooms);
+    map.showMarkers(washrooms);
   }
 
   @OnClick(R.id.fab_game) void onFabGameClick() {
     ArrayList<Place> gameLocations = presenter.getPlacesContainingString(places, PLACE_GAMING);
-    showMarkers(gameLocations);
+    map.showMarkers(gameLocations);
   }
 
   @OnClick(R.id.fab_food) void onFabFoodClick() {
     ArrayList<Place> foodBooths = presenter.getPlacesContainingString(places, PLACE_FOOD);
-    showMarkers(foodBooths);
+    map.showMarkers(foodBooths);
   }
 
   @OnClick(R.id.direction_button) void onDirectionButtonClick() {
-    Position origin =
-        Position.fromCoordinates(userLocation.getLongitude(), userLocation.getLatitude());
-    Position destination = Position.fromCoordinates(currentMarker.getPosition().getLongitude(),
-        currentMarker.getPosition().getLatitude());
-    getRoute(origin, destination);
+    map.showRoute();
   }
 
   @OnClick(R.id.fab_location) void onFabLocationClick() {
-    showMarkerOnUserLocation();
-    animateCameraToPosition(userLocation.getLatLng());
-  }
-
-  private void showMarkerOnUserLocation() {
-    IconFactory iconFactory = IconFactory.getInstance(getContext());
-    Icon icon = iconFactory.fromResource(R.drawable.ic_marker_blue);
-    Marker marker = mapboxMap.addMarker(new MarkerOptions().position(userLocation.getLatLng())
-        .title(userLocation.getName())
-        .icon(icon));
-    markers.add(marker);
-  }
-
-  private void animateCameraToPosition(LatLng latLng) {
-    CameraPosition position = new CameraPosition.Builder().target(latLng).tilt(30).build();
-    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
-  }
-
-  private void getRoute(Position origin, Position destination) {
-    MapboxDirections client =
-        new MapboxDirections.Builder().setAccessToken(getString(R.string.mapbox_access_token))
-            .setOrigin(origin)
-            .setDestination(destination)
-            .setOverview(DirectionsCriteria.OVERVIEW_FULL)
-            .setProfile(DirectionsCriteria.PROFILE_WALKING)
-            .setAnnotation(DirectionsCriteria.ANNOTATION_DISTANCE,
-                DirectionsCriteria.ANNOTATION_DURATION)
-            .build();
-
-    client.enqueueCall(new Callback<DirectionsResponse>() {
-      @Override
-      public void onResponse(Call<DirectionsResponse> call, Response<DirectionsResponse> response) {
-        Log.d(TAG, call.request().url().toString());
-        if (response.body() == null || response.body().getRoutes().size() < 1) {
-          Log.e(TAG, "No routes found, make sure you set the right user and access token.");
-          return;
-        }
-        currentRoute = response.body().getRoutes().get(0);
-        Toast.makeText(getContext(), "Distance: " + currentRoute.getDistance(), Toast.LENGTH_SHORT)
-            .show();
-        drawRoute(currentRoute);
-      }
-
-      @Override public void onFailure(Call<DirectionsResponse> call, Throwable throwable) {
-        Log.e(TAG, "Error: " + throwable.getMessage());
-        Toast.makeText(getContext(), "Error: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
-      }
-    });
-  }
-
-  private void drawRoute(DirectionsRoute route) {
-    slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
-    // Convert LineString coordinates into LatLng[]
-    LineString lineString = LineString.fromPolyline(route.getGeometry(), PRECISION_6);
-    List<Position> coordinates = lineString.getCoordinates();
-    LatLng[] points = new LatLng[coordinates.size()];
-    for (int i = 0; i < coordinates.size(); i++) {
-      points[i] = new LatLng(coordinates.get(i).getLatitude(), coordinates.get(i).getLongitude());
-    }
-
-    if (currentRoutePolyline != null) mapboxMap.removePolyline(currentRoutePolyline);
-    currentRoutePolyline = mapboxMap.addPolyline(
-        new PolylineOptions().add(points).color(Color.parseColor("#007cbf")).width(3));
-    showMarkerOnUserLocation();
-    ArrayList<LatLng> locations = new ArrayList<>();
-    locations.add(userLocation.getLatLng());
-    locations.add(currentMarker.getPosition());
-    fitCameraWithinBounds(locations);
-  }
-
-  private void showMarkers(ArrayList<Place> placeList) {
-    clearMarkers();
-    ArrayList<LatLng> locations = new ArrayList<>();
-    for (Place place : placeList) {
-      locations.add(place.getLatLng());
-      Marker marker = mapboxMap.addMarker(
-          new MarkerOptions().position(place.getLatLng()).title(place.getName()));
-      markers.add(marker);
-    }
-    fitCameraWithinBounds(locations);
-  }
-
-  private void fitCameraWithinBounds(ArrayList<LatLng> locations) {
-    LatLngBounds.Builder latLngBoundsBuilder = new LatLngBounds.Builder();
-    for (LatLng location : locations) {
-      latLngBoundsBuilder.include(location);
-    }
-    LatLngBounds latLngBounds = latLngBoundsBuilder.build();
-    mapboxMap.easeCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 250), 2000);
-  }
-
-  private void clearMarkers() {
-    for (Marker marker : markers) {
-      mapboxMap.removeMarker(marker);
-    }
+    map.showMarkerOnUserLocation();
+    map.animateCameraToPosition(map.userLocation.getLatLng());
   }
 
   private void expandFabMenu() {
@@ -329,19 +191,13 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
 
   private void collapseFabMenu() {
     isFabOpen = false;
-    clearMarkers();
+    map.clearMarkers();
     fab.setImageResource(R.drawable.ic_fab_expand);
     fab.setBackgroundTintList(
         ColorStateList.valueOf(ContextCompat.getColor(getActivity(), R.color.colorAccent)));
     fabWashroom.animate().translationX(0);
     fabGame.animate().translationX(0);
     fabFood.animate().translationX(0);
-  }
-
-  private void setCurrentLocation() {
-    userLocation.setLatitude(USER_LATITUDE);
-    userLocation.setLongitude(USER_LONGITUDE);
-    userLocation.setName(CURRENT_LOCATION);
   }
 
   private SlidingUpPanelLayout.PanelSlideListener onSlideListener() {
@@ -359,6 +215,21 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
     };
   }
 
+  @Override public void onDrawRoute() {
+    slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.HIDDEN);
+  }
+
+  @Override public void onMarkerClicked(String title) {
+    locationText.setText(title);
+    directionButton.setVisibility(View.VISIBLE);
+    if (slidingLayout.getPanelState() == SlidingUpPanelLayout.PanelState.HIDDEN) {
+      slidingLayout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED);
+    }
+    if (CURRENT_LOCATION.equalsIgnoreCase(title)) {
+      directionButton.setVisibility(View.GONE);
+    }
+  }
+
   private class DrawTrackGeoJson extends AsyncTask<Void, Void, List<LatLng>> {
 
     @Override protected List<LatLng> doInBackground(Void... voids) {
@@ -369,9 +240,7 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
     @Override protected void onPostExecute(List<LatLng> points) {
       super.onPostExecute(points);
       if (points.size() > 0) {
-        mapboxMap.addPolyline(new PolylineOptions().addAll(points)
-            .color(ContextCompat.getColor(getActivity(), R.color.white))
-            .width(4));
+        map.addPolyline(R.color.white, 4, points);
       }
     }
   }
@@ -386,9 +255,7 @@ public class MapFragment extends Fragment implements MapContract.View, OnMapRead
     @Override protected void onPostExecute(List<LatLng> points) {
       super.onPostExecute(points);
       if (points.size() > 0) {
-        mapboxMap.addPolyline(new PolylineOptions().addAll(points)
-            .color(ContextCompat.getColor(getActivity(), R.color.colorAccent))
-            .width(4));
+        map.addPolyline(R.color.colorAccent, 4, points);
       }
     }
   }
