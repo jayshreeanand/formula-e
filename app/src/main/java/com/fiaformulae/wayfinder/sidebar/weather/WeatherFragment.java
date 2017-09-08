687839755774a41469d9f29b712758acd4c77e71
Naveen <naveen246@gmail.com>
2017-09-08 21:27:11 +0530

    Call weather endpoint

--- app/src/main/java/com/fiaformulae/wayfinder/sidebar/weather/WeatherFragment.java
+++ app/src/main/java/com/fiaformulae/wayfinder/sidebar/weather/WeatherFragment.java
@@ -6,11 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.fiaformulae.wayfinder.MainActivity;
 import com.fiaformulae.wayfinder.R;
+import com.fiaformulae.wayfinder.models.Weather;
+import java.text.DecimalFormat;
 
 public class WeatherFragment extends Fragment implements WeatherContract.View {
+  @BindView(R.id.temperature) TextView temperatureView;
+  @BindView(R.id.progress_bar) ProgressBar progressBar;
   private WeatherContract.Presenter presenter;
 
   @Nullable @Override
@@ -24,10 +31,25 @@ public class WeatherFragment extends Fragment implements WeatherContract.View {
     ButterKnife.bind(this, view);
     ((MainActivity) getActivity()).showToolbar();
     presenter = new WeatherPresenter(this);
+    temperatureView.setText("");
+    presenter.getWeather();
   }
 
   @Override public void onDestroy() {
     super.onDestroy();
     presenter.onDestroy();
+  }
+
+  @Override public void showProgressbar() {
+    progressBar.setVisibility(View.VISIBLE);
+  }
+
+  @Override public void hideProgresbar() {
+    progressBar.setVisibility(View.GONE);
+  }
+
+  @Override public void onGettingWeather(Weather weather) {
+    DecimalFormat df = new DecimalFormat("#.#");
+    temperatureView.setText(df.format(weather.getCurrentTemperature()) + " C");
   }
 }
--- app/src/main/java/com/fiaformulae/wayfinder/sidebar/weather/WeatherPresenter.java
+++ app/src/main/java/com/fiaformulae/wayfinder/sidebar/weather/WeatherPresenter.java
@@ -1,10 +1,17 @@
 package com.fiaformulae.wayfinder.sidebar.weather;
 
+import android.util.Log;
+import com.fiaformulae.wayfinder.models.Weather;
 import com.fiaformulae.wayfinder.network.WayfinderApi;
 import com.fiaformulae.wayfinder.utils.RxUtils;
+import rx.observables.ConnectableObservable;
 import rx.subscriptions.CompositeSubscription;
 
+import static com.fiaformulae.wayfinder.AppConstants.USER_LATITUDE;
+import static com.fiaformulae.wayfinder.AppConstants.USER_LONGITUDE;
+
 public class WeatherPresenter implements WeatherContract.Presenter {
+  private static final String TAG = "WeatherPresenter";
   private WeatherContract.View view;
   private CompositeSubscription compositeSubscription;
   private WayfinderApi wayfinderApi;
@@ -17,5 +24,24 @@ public class WeatherPresenter implements WeatherContract.Presenter {
 
   @Override public void onDestroy() {
     RxUtils.clearCompositeSubscription(compositeSubscription);
+  }
+
+  @Override public void getWeather() {
+    view.showProgressbar();
+    ConnectableObservable<Weather> observable =
+        wayfinderApi.getWeather(USER_LATITUDE, USER_LONGITUDE);
+    compositeSubscription.add(observable.connect());
+    compositeSubscription.add(
+        observable.subscribe(this::onGetWeatherSuccess, this::onGetWeatherFailure));
+  }
+
+  private void onGetWeatherSuccess(Weather weather) {
+    view.hideProgresbar();
+    view.onGettingWeather(weather);
+  }
+
+  private void onGetWeatherFailure(Throwable throwable) {
+    view.hideProgresbar();
+    Log.d(TAG, throwable.getMessage());
   }
 }
